ca2101451c2dd71a2382530c90b182feff161847
PriadiCuanda-231401058 <151598769+PriadiCuanda-231401058@users.noreply.github.com>
2025-10-17 21:13:45 +0700

    revisi regis

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 
 import 'package:fittrack/features/auth/screens/register_screen.dart';
 import 'package:flutter/material.dart';
-
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
