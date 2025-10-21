4f79a363553107d0e316c6ca7e36168b1e2cc508
PriadiCuanda-231401058 <151598769+PriadiCuanda-231401058@users.noreply.github.com>
2025-10-21 22:49:55 +0700

    fitur sign in with google dan sign out nya, mungkin blm sempurna

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:fittrack/features/home/home_controller.dart';
+import 'package:fittrack/models/user_model.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+// import 'package:fittrack/features/auth/auth_controller.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,12 +13,62 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final HomeController _homeController = HomeController();
+  UserModel? userModel;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadUser();
+  }
+
+  Future<void> _loadUser() async {
+    final user = await _homeController.getCurrentUserData();
+    setState(() {
+      userModel = user;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
-        child: Text('Home Screen'),
-      ),
-    );
+    return Scaffold(
+  appBar: AppBar(title: const Text('Home')),
+  body: userModel == null
+      ? const Center(child: CircularProgressIndicator())
+      : Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '👋 Halo, ${userModel!.name}',
+                style: const TextStyle(fontSize: 20),
+              ),
+              Text('Email: ${userModel!.email}'),
+              Text('UID: ${userModel!.uid}'),
+
+              const SizedBox(height: 24),
+
+              // Tombol Ganti Akun
+              TextButton(
+                onPressed: () async {
+                  await GoogleSignIn().signOut();
+                  await FirebaseAuth.instance.signOut();
+                  
+                  // Kembali ke halaman login
+                  if (context.mounted) {
+                    Navigator.pushReplacementNamed(context, '/loginScreen');
+                  }
+                },
+                child: const Text(
+                  "Ganti akun",
+                  style: TextStyle(fontSize: 16, color: Colors.red),
+                ),
+              ),
+            ],
+          ),
+        ),
+);
+
   }
 }
