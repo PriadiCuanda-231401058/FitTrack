b05de64b52a6c53b0280f9ae2614ebe574c86b1c
Andre <andrelim865@gmail.com>
2025-11-11 22:50:08 +0700

    feat: add splash screen

---
+++ lib/features/splash/splash_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+
+    Future.delayed(const Duration(seconds: 3), () {
+      if (!mounted) return;
+      Navigator.pushReplacementNamed(context, '/loginScreen');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Material(
+      child: Container(
+        width: screenWidth,
+        height: screenHeight,
+        color: Colors.black,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/fittrack_logo.png',
+              width: screenWidth * 0.2,
+            ),
+
+            SizedBox(width: screenWidth * 0.01),
+
+            Text(
+              'FitTrack',
+              style: TextStyle(
+                fontFamily: 'LeagueSpartan',
+                fontSize: screenWidth * 0.09,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
