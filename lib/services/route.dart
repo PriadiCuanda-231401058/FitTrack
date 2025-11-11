45fc5cd2176ea1ea350c1ef6d9bafc88ae5dc7ce
Andre <andrelim865@gmail.com>
2025-11-11 22:50:08 +0700

    feat: add splash screen

--- lib/services/route.dart
+++ lib/services/route.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:fittrack/features/splash/splash_screen.dart';
 import 'package:fittrack/features/auth/screens/login_screen.dart';
 import 'package:fittrack/features/auth/screens/register_screen.dart';
 import 'package:fittrack/features/home/home_screen.dart';
 
 final Map<String, WidgetBuilder> appRoutes = {
-  '/': (context) => const RegisterScreen(),
+  '/': (context) => const SplashScreen(),
   '/loginScreen': (context) => const LoginScreen(),
+  '/registerScreen': (context) => const RegisterScreen(),
   '/homeScreen': (context) => const HomeScreen(),
 };
